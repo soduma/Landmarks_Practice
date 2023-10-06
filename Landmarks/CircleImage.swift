10eb2abf3b682001e98081965c3bc091dca1b90d
JANG KIHWA <soduma@naver.com>
2023-10-06 16:13:27 +0900

    Creating and combining views

---
+++ Landmarks/CircleImage.swift
@@ -0,0 +1,18 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by 장기화 on 10/6/23.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image(uiImage: "turtlerock")
+    }
+}
+
+#Preview {
+    CircleImageView()
+}
---
+++ Landmarks/MapView.swift
@@ -0,0 +1,18 @@
+//
+//  MapView.swift
+//  Landmarks
+//
+//  Created by 장기화 on 10/6/23.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MapView()
+}
