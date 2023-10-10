e97dc736e18abefd9c4bbfd59726a1ea02c3d118
JANG KIHWA <soduma@naver.com>
2023-10-10 18:15:57 +0900

    Animating Views and Transitions

---
+++ Landmarks/Model/Hike.swift
@@ -0,0 +1,30 @@
+//
+//  Hike.swift
+//  Landmarks
+//
+//  Created by 장기화 on 10/10/23.
+//
+
+import Foundation
+
+struct Hike: Codable, Hashable, Identifiable {
+    var id: Int
+    var name: String
+    var distance: Double
+    var difficulty: Int
+    var observations: [Observation]
+    
+    static var formatter = LengthFormatter()
+    
+    var distanceText: String {
+        Hike.formatter.string(fromValue: distance, unit: .kilometer)
+    }
+    
+    struct Observation: Codable, Hashable {
+        var distanceFromStart: Double
+        
+        var elevation: Range<Double>
+        var pace: Range<Double>
+        var heartRate: Range<Double>
+    }
+}
