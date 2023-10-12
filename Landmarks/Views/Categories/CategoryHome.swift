4e6229eb375ac45b11bd79c9c356a13fc92ffd18
JANG KIHWA <soduma@naver.com>
2023-10-12 16:46:50 +0900

    Working with Ui Controls

--- Landmarks/Views/Categories/CategoryHome.swift
+++ Landmarks/Views/Categories/CategoryHome.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct CategoryHome: View {
     @Environment(ModelData.self) var modelData
+    @State private var showingProfile = false
     
     var body: some View {
         NavigationView {
@@ -25,8 +26,19 @@ struct CategoryHome: View {
                 }
                 .listRowInsets(EdgeInsets())
             }
+            .listStyle(.inset)
             .navigationTitle("Featured")
-//            .listStyle(.inset)
+            .toolbar {
+                Button(action: {
+                    showingProfile.toggle()
+                }, label: {
+                    Label("User Profile", systemImage: "person.crop.circle")
+                })
+            }
+            .sheet(isPresented: $showingProfile, content: {
+                ProfileHost()
+                    .environment(modelData)
+            })
         }
     }
 }
